2fe2267a1ab8d7bfcacf3cbc05d5597792de36d3
Jmartinezgranados <Jmartinezgranados@gmail.com>
2020-08-14 12:34:25 +0200

    conectar mongo

--- FlaskPolls/FlaskPolls/settings.py
+++ FlaskPolls/FlaskPolls/settings.py
@@ -7,7 +7,8 @@ environment variables, or set the default values in code here.
 
 from os import environ
 
-REPOSITORY_NAME = environ.get('REPOSITORY_NAME', 'memory')
+#REPOSITORY_NAME = environ.get('REPOSITORY_NAME', 'memory')
+REPOSITORY_NAME = environ.get('REPOSITORY_NAME', 'mongodb')
 
 if REPOSITORY_NAME == 'azuretablestorage':
     REPOSITORY_SETTINGS = {
@@ -18,7 +19,8 @@ if REPOSITORY_NAME == 'azuretablestorage':
     }
 elif REPOSITORY_NAME == 'mongodb':
     REPOSITORY_SETTINGS = {
-        'MONGODB_HOST': environ.get('MONGODB_HOST', None),
+        #'MONGODB_HOST': environ.get('MONGODB_HOST', None),
+        'MONGODB_HOST': environ.get('MONGODB_HOST', '52.47.123.236'),
         'MONGODB_DATABASE': environ.get('MONGODB_DATABASE', 'polls'),
         'MONGODB_COLLECTION': environ.get('MONGODB_COLLECTION', 'polls'),
     }
